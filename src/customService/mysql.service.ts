ad4fd200f6bb79a5f6d75549cd34a630f087c90a
karadenizemirr <karadenizemirr@gmail.com>
2023-09-07 00:39:46 +0300

    Database Update

--- src/customService/mysql.service.ts
+++ src/customService/mysql.service.ts
@@ -6,11 +6,11 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: "spryrr1myu6oalwl.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
     port: 3306,
-    username: "root",
-    password: "123456789",
-    database: "slotabimbot",
+    username: "k6cmh8q1hxlz3qkp",
+    password: "wa5qfwy66og5b78q",
+    database: "pz6nl117rkr41hgi",
     synchronize: true,
     logging: true,
     entities: [Website, Message, Settings, Book],
